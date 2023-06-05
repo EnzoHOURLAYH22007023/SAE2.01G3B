ca3f494d5ca5f36a16dcb2473eb4973d39c10881
Enzo <enzo.hourlay@etu.univ-amu.fr>
2023-06-05 11:25:26 +0200

    Init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.sae201g3b {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.sae201g3b to javafx.fxml;
+    exports com.sae201g3b;
+}
