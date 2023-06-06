f867c2f82cd6db92ed66b30af235b89b15c2ad37
Enzo <enzo.hourlay@etu.univ-amu.fr>
2023-06-06 11:44:05 +0200

    ajout : Map opérationnel

--- src/main/java/com/sae201g3b/SisMapController.java
+++ src/main/java/com/sae201g3b/SisMapController.java
@@ -6,6 +6,16 @@ import com.gluonhq.maps.MapView;
 
 public class SisMapController {
     @FXML
-    private MapView map;
-    MapPoint mapPoint = new MapPoint(46.227638, 2.213749);
+    private MapView france;
+    public void initialize(){
+        System.setProperty("javafx.platform", "desktop");
+        System.setProperty("http.agent", "Gluon Mobile/1.0.3");
+
+        MapPoint francePoint = new MapPoint(46.227638, 2.213749);
+
+        france.setZoom(6);
+        france.flyTo(0,francePoint,0.1);
+
+        france.setDisable(true);
+    }
  }
