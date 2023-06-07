625c86b8f89d5415106eb91cf50d344379df6c1f
Massko <enzo.hourlay@etu.univ-amu.fr>
2023-06-07 18:10:11 +0200

    feat : possibilité de mettre un point rouge sur la carte

--- src/main/java/com/sae201g3b/SisMapController.java
+++ src/main/java/com/sae201g3b/SisMapController.java
@@ -1,5 +1,6 @@
 package com.sae201g3b;
 
+import com.gluonhq.maps.MapLayer;
 import javafx.fxml.FXML;
 import com.gluonhq.maps.MapPoint;
 import com.gluonhq.maps.MapView;
@@ -7,15 +8,19 @@ import com.gluonhq.maps.MapView;
 public class SisMapController {
     @FXML
     private MapView france;
+    private CustomCircleMarkerLayer pointLayer;
     public void initialize(){
         System.setProperty("javafx.platform", "desktop");
         System.setProperty("http.agent", "Gluon Mobile/1.0.3");
 
+        /* La fonction est appelée à chaque rafraichissement de la carte */
         MapPoint francePoint = new MapPoint(46.227638, 2.213749);
-
+        System.out.println(francePoint.getLatitude());
         france.setZoom(6);
         france.flyTo(0,francePoint,0.1);
-
         france.setDisable(true);
+
+        pointLayer = new CustomCircleMarkerLayer(francePoint);
+        france.addLayer(pointLayer);
     }
  }
