851dc4b5aa47bf839e1a9d0299da2ccbc83663c4
Massko <enzo.hourlay@etu.univ-amu.fr>
2023-06-12 23:53:18 +0200

    add : LineChart affiche les données

--- src/main/java/com/sae201g3b/DashboardController.java
+++ src/main/java/com/sae201g3b/DashboardController.java
@@ -8,18 +8,41 @@
  */
 package com.sae201g3b;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
+
+import static com.sae201g3b.Database.*;
 
 public class DashboardController {
     @FXML
     private BorderPane borderPane;
+    @FXML
+    private LineChart lineChart;
+    private List<Seisme> data;
+    private List<Seisme> datafiltrer;
     public void initialize(){
+        Database CSV = new Database();
+        ImportCSV();
+        data = getData();
+        datafiltrer = getDataFiltrer();
+
+        ObservableList<XYChart.Data<String,Number>> tmp = FXCollections.observableArrayList();
+        for (Seisme seisme : data) {
+            Float intensite = Float.parseFloat(seisme.getIntensite());
+            String antmp = seisme.getDate().substring(0,4);
+            tmp.add(new XYChart.Data<>(antmp, intensite));
+        }
+        lineChart.setData(FXCollections.observableArrayList(new XYChart.Series<>(tmp)));
     }
     @FXML
     public void changerFXML() {
@@ -31,6 +54,11 @@ public class DashboardController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void appliquer(){
+
     }
 
 }
